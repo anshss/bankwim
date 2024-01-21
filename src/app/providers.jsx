f08603d066c5ca04757df114f056e8e782d6aaa1
Ansh <anshsaxena4190@gmail.com>
2024-01-21 08:53:40 +0530

    feat: added wallet integrations

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,12 +1,40 @@
-// app/providers.jsx
-'use client'
+"use client";
 
-import { NextUIProvider } from '@nextui-org/react'
+import * as React from "react";
+import { WagmiConfig, createConfig } from "wagmi";
+import {
+    ConnectKitProvider,
+    getDefaultConfig,
+} from "connectkit";
+import { mainnet, polygon, polygonMumbai, sepolia } from "wagmi/chains";
+
+const chains = [mainnet, polygon, polygonMumbai, sepolia];
+
+const config = createConfig(
+    getDefaultConfig({
+        // Required API Keys
+        alchemyId: process.env.ALCHEMY_ID, // or infuraId
+        walletConnectProjectId: "85a616505f219621a73d1af8a208fd14",
+
+        // Required
+        appName: "Bankwim",
+
+        chains,
+
+        // Optional
+        appDescription: "Your App Description",
+        appUrl: "https://family.co", // your app's url
+        appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+    })
+);
 
 export function Providers({ children }) {
-  return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
-  )
+    const [mounted, setMounted] = React.useState(false);
+    React.useEffect(() => setMounted(true), []);
+    
+    return (
+        <WagmiConfig config={config}>
+            <ConnectKitProvider>{mounted && children}</ConnectKitProvider>
+        </WagmiConfig>
+    );
 }
