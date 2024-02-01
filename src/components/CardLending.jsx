e99133d0988ed91ccc8679bb86e69a1b334ac47b
Akash Biswas <akash8860biswas@gmail.com>
2024-02-01 17:03:45 +0530

    fetching nfts in lending

--- src/components/CardLending.jsx
+++ src/components/CardLending.jsx
@@ -1,17 +1,41 @@
 import { useState } from 'react';
 import styles from '../styles/collateral.module.css'
+import axios from 'axios';
 
 export function Card(prop) {
     const [dataInput, setData] = useState({ 
         value: "", 
         term: "" 
-      })
-      
+      });
+    
+      const [nftUrl, setNftUrl] = useState();
+      console.log(prop.uri)
+    
+      const fetchData = async (url) => {
+        try {
+          const ipfsUrl = url;
+          const uslObj = { ipfsUri: ipfsUrl };
+          const response = await axios.post("/api/uriHandler/", uslObj);
+    
+          const imageUrl = response.data.ipfsData.image;
+          return imageUrl;
+    
+          console.log("Data from IPFS:", data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+    
+      const imageFromUri = fetchData(prop.uri).then((value) => {
+        console.log(setNftUrl(value));
+      });
+
     return (
       <div className={styles.card}>
         <img 
         className="w-full p-4 border border-white border-2 rounded-sm"
-        src={prop.uri} />
+        src={nftUrl} />
+        
         <div className="flex max-w-[400px] justify-around gap-1.5">
           <input
             className="mt-3 bg-white items-center text-center text-xl font-normal border-0 text-black p-2.5 w-[180px] rounded-md"
